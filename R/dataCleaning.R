fc668a34301af8c8e6df9d93857530926be34549
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-02-20 21:10:06 +0300

    Clean electricty data for each district

--- R/dataCleaning.R
+++ R/dataCleaning.R
@@ -9,3 +9,26 @@ silivri <- read_excel("data/airQuality/european/silivrimthm.xlsx", skip = 1)
 arnavutkoy <- read_excel("data/airQuality/european/arnavutkoy.xlsx", skip = 1)
 sultangazi <- read_excel("data/airQuality/european/sultangazimthm.xlsx", skip = 1)
 
+uskudar <- uskudar %>%
+  filter(!apply(uskudar, 1, function(row) any(row == "-"))) %>%
+  mutate(across((ncol(.)-3):ncol(.), ~ as.numeric(gsub(",", ".", gsub("[^0-9.,-]", "", .)))))
+
+kartal <- kartal %>%
+  filter(!apply(kartal, 1, function(row) any(row == "-"))) %>%
+  mutate(across((ncol(.)-3):ncol(.), ~ as.numeric(gsub(",", ".", gsub("[^0-9.,-]", "", .)))))
+
+umraniye <- umraniye %>%
+  filter(!apply(umraniye, 1, function(row) any(row == "-"))) %>%
+  mutate(across((ncol(.)-3):ncol(.), ~ as.numeric(gsub(",", ".", gsub("[^0-9.,-]", "", .)))))
+
+silivri <- silivri %>%
+  filter(!apply(silivri, 1, function(row) any(row == "-"))) %>%
+  mutate(across((ncol(.)-3):ncol(.), ~ as.numeric(gsub(",", ".", gsub("[^0-9.,-]", "", .)))))
+
+arnavutkoy <- arnavutkoy %>%
+  filter(!apply(arnavutkoy, 1, function(row) any(row == "-"))) %>%
+  mutate(across((ncol(.)-3):ncol(.), ~ as.numeric(gsub(",", ".", gsub("[^0-9.,-]", "", .)))))
+
+sultangazi <- sultangazi %>%
+  filter(!apply(sultangazi, 1, function(row) any(row == "-"))) %>%
+  mutate(across((ncol(.)-3):ncol(.), ~ as.numeric(gsub(",", ".", gsub("[^0-9.,-]", "", .)))))
